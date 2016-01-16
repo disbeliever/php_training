56966afc652816166df37e14c23b1ba8c7719613
disbeliever <nerevar101@mail.ru>
2016-01-16 21:42:45 +0400

    More client-side validation in ViewStudent

--- students/views/ViewStudent.php
+++ students/views/ViewStudent.php
@@ -38,7 +38,7 @@
 
           <div class="form-group">
             <label for="mark">Баллы</label>
-            <input class="form-control" name="mark" type="number" min="0" max="300" value="<?=$student->mark?>"/>
+            <input class="form-control" name="mark" type="number" min="0" max="300" required value="<?=$student->mark?>"/>
           </div>
 
           <div class="form-group">
@@ -53,12 +53,12 @@
 
           <div class="form-group">
             <label for="birthyear">Год рождения</label>
-            <input class="form-control" name="birthyear" type="number" min="1900" max="2000" value="<?=$student->birthyear?>"/>
+            <input class="form-control" name="birthyear" type="number" min="1900" max="2000" required value="<?=$student->birthyear?>"/>
           </div>
 
           <div class="form-group">
             <label for="email">E-mail</label>
-            <input class="form-control" name="email" type="email" maxlength="254" value="<?=htmlspecialchars($student->email)?>"/>
+            <input class="form-control" name="email" type="email" maxlength="254" required value="<?=htmlspecialchars($student->email)?>"/>
           </div>
 
           <button class="btn btn-default" type="submit" name="dosave" value="1"><?=$saveButtonText?></button>
