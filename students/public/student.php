213d4f8982058efaf19f2cc42d0c1d59fbc0a8c1
disbeliever <nerevar101@mail.ru>
2016-02-07 19:25:49 +0400

    students: move code from student.php to ControllerStudent class better error handling in student.php

--- students/public/student.php
+++ students/public/student.php
@@ -2,85 +2,13 @@
 require_once('../src/init.php');
 require_once('../src/registerHelpers.php');
 
-$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-$registered = isset($_GET['registered']);
-$changesSaved = isset($_GET['changesSaved']);
-$token = getOrGenerateCSRFToken();
+$app = new ControllerStudent($stg);
 
-if ($registered) {
-    $msg = ["class" => "success", "text" => "Регистрация выполнена"];
+try {
+    $app->run();
 }
-else if ($changesSaved) {
-    $msg = ["class" => "success", "text" => "Изменения сохранены"];
-}
-
-/* if method is POST then fill a Student object with data from $_POST
-   and try to save it */
-if (isFormSent()) {
-    if (isset($_COOKIE['auth'])) {
-        $student = $stg->getStudent($_COOKIE['auth']);
-    } else {
-        $student = new Student();
-    }
-    updateStudentFromPostAndCookies($student);
-
-    if (!(isCSRFTokenSet() && isFormTokenSet() && $_COOKIE['csrf'] == $_POST['csrfToken'])) {
-        $msg = ["class" => "danger", "text" => "Ошибка. Попробуйте сохранить данные ещё раз"];
-    }
-    else {
-        $validator = new StudentValidator($stg);
-        $errors = $validator->validate($student);
-
-        if (count($errors) == 0) {
-            try {
-                if (isEditable()) {
-                    $redirectSuffix = "&changesSaved=1";
-                    $stg->updateStudent($student);
-                }
-                else {
-                    $redirectSuffix = "&registered=1";
-                    $student->auth = generateRandomString();
-                    $stg->addStudent($student);
-                    setcookie('auth', $student->auth, time() + 10*365*24*60*60, '/', null, false, true);
-                }
-                $redirectTo = "{$_SERVER['SCRIPT_NAME']}?id={$student->id}$redirectSuffix";
-                header("Location: $redirectTo");
-            }
-            catch (PDOException $e) {
-                #TODO: actually do something here
-                var_dump($e);
-                #$errString = ;
-            }
-        }
-    }
-}
-/* if method is GET then we should retreive student from DB (if we got id)
-   or create a new one. */
-else if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    if ($id > 0) {
-        $student = $stg->getStudent($id);
-    }
-    else if (isset($_COOKIE['auth'])) {
-        $student = $stg->getStudent($_COOKIE['auth']);
-    }
-    else {
-        $student = new Student();
-    }
-}
-
-if ($student != null) {
-    if ((isset($student->id) && $student->id > 0) || isset($_COOKIE['auth'])) {
-        $title = "Студент: $student->firstName $student->lastName";
-        $saveButtonText = "Сохранить изменения";
-    }
-    else {
-        $title = "Регистрация";
-        $saveButtonText = "Зарегистрироваться";
-    }
-    include('../src/views/ViewStudent.php');
-}
-else {
-    header("HTTP/1.0 404 Student not found");
-    $errString = "Абитуриент с id=$id не найден";
+catch(Exception $e) {
+    $errString = "Упс, что-то пошло не так :(";
     include('../src/views/404.php');
+    error_log($e);
 }
---
+++ students/src/ControllerStudent.php
@@ -0,0 +1,88 @@
+<?php
+class ControllerStudent
+{
+    public function __construct($stg)
+    {
+        $this->stg = $stg;
+    }
+
+    public function run()
+    {
+        $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+        $registered = isset($_GET['registered']);
+        $changesSaved = isset($_GET['changesSaved']);
+        $token = getOrGenerateCSRFToken();
+
+        if ($registered) {
+            $msg = ["class" => "success", "text" => "Регистрация выполнена"];
+        }
+        else if ($changesSaved) {
+            $msg = ["class" => "success", "text" => "Изменения сохранены"];
+        }
+
+        /* if method is POST then load Student from DB, update it with data
+           from $_POST and try to save it */
+        if (isFormSent()) {
+            if (isset($_COOKIE['auth'])) {
+                $student = $this->stg->getStudent($_COOKIE['auth']);
+            } else {
+                $student = new Student();
+            }
+            updateStudentFromPostAndCookies($student);
+
+            if (!(isCSRFTokenSet() && isFormTokenSet() && $_COOKIE['csrf'] == $_POST['csrfToken'])) {
+                $msg = ["class" => "danger", "text" => "Ошибка. Попробуйте сохранить данные ещё раз"];
+            }
+            else {
+                $validator = new StudentValidator($this->stg);
+                $errors = $validator->validate($student);
+
+                if (count($errors) == 0) {
+                    if (isEditable()) {
+                        $redirectSuffix = "&changesSaved=1";
+                        $this->stg->updateStudent($student);
+                    }
+                    else {
+                        $redirectSuffix = "&registered=1";
+                        $student->auth = generateRandomString();
+                        $this->stg->addStudent($student);
+                        setcookie('auth', $student->auth, time() + 10*365*24*60*60, '/', null, false, true);
+                    }
+                    $redirectTo = "{$_SERVER['SCRIPT_NAME']}?id={$student->id}$redirectSuffix";
+                    header("Location: $redirectTo");
+                }
+            }
+        }
+        /* if method is GET then we should retreive student from DB (if we got id)
+           or create a new one. */
+        else if ($_SERVER['REQUEST_METHOD'] == "GET") {
+            if ($id > 0) {
+                $student = $this->stg->getStudent($id);
+            }
+            else if (isset($_COOKIE['auth'])) {
+                $student = $this->stg->getStudent($_COOKIE['auth']);
+            }
+            else {
+                $student = new Student();
+            }
+        }
+
+        if ($student != null) {
+            if ((isset($student->id) && $student->id > 0) || isset($_COOKIE['auth'])) {
+                $title = "Студент: $student->firstName $student->lastName";
+                $saveButtonText = "Сохранить изменения";
+            }
+            else {
+                $title = "Регистрация";
+                $saveButtonText = "Зарегистрироваться";
+            }
+            include('../src/views/ViewStudent.php');
+        }
+        else {
+            header("HTTP/1.0 404 Student not found");
+            $errString = "Абитуриент с id=$id не найден";
+            include('../src/views/404.php');
+        }
+
+    }
+}
