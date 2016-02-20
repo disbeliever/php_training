54bf591c6436d339570707e2514fe780ef700179
disbeliever <nerevar101@mail.ru>
2016-02-20 21:07:00 +0400

    filehosting: use twig templates.

--- filehosting/public/index.php
+++ filehosting/public/index.php
@@ -2,10 +2,24 @@
 require '../vendor/autoload.php';
 
 $app = new \Slim\App();
-$app->get('/', function() {
-    include('../src/views/index.phtml'); 
+
+$container = $app->getContainer();
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('../src/views', [
+        'cache' => false
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+};
+
+$app->get('/', function($request, $response, $args) {
+    return $this->view->render($response, 'index.html', array());
 });
-$app->get('/last/', function() {
-    include('../src/views/last.phtml');
+$app->get('/last/', function($request, $response, $args) {
+    return $this->view->render($response, 'last.html', array());
 });
 $app->run();
